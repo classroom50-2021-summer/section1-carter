7a1cfada428dbaac2eeba9a660c5566486fbb404
Carter Zenke <5281090+CarterZenke@users.noreply.github.com>
2021-06-24 17:22:43 +0000

    add types.c

---
+++ types.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    // Values to represent:
+    // Height of a pyramid
+    // A hash (#)
+    // Currency value
+    // Value of a quarter
+    // Value of a dime
+    // Credit card number
+
+    // Initialize the above values using the following syntax:
+    // [data_type] [name] = [value];
+
+    // Print them using their corresponding format code:
+    printf("Height: \n");
+    printf("Hash: \n");
+    printf("Currency: \n");
+    printf("Quarter value: \n");
+    printf("Dime value: \n");
+    printf("Credit card number: \n");
+}
